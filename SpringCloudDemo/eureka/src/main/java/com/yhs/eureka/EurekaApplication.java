3ea52973d45dc40ce957b3edb426fc34db90fd8f
袁鸿帅 <hongshuai.yuan@hand-china.com>
2019-04-08 15:02:13 +0800

    修改eureka服务端口

--- SpringCloudDemo/eureka/src/main/java/com/yhs/eureka/EurekaApplication.java
+++ SpringCloudDemo/eureka/src/main/java/com/yhs/eureka/EurekaApplication.java
@@ -3,11 +3,9 @@ package com.yhs.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
-@PropertySource("classpath:application.yml")
 public class EurekaApplication {
 
     public static void main(String[] args) {
